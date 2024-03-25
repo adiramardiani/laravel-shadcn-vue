9b3cee64f09343e325e6d31847529291273a4016
adiramardiani <adira.mardiani@gmail.com>
2024-03-25 18:00:57 +0700

    Add initial filament configuration

--- README.md
+++ README.md
@@ -64,3 +64,11 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Running Queue
+
+php artisan queue:listen --memory=2048 --timeout=16000 --queue=high,medium,low,default
+
+## Reset Cache
+
+php artisan permission:cache-reset
---
+++ app/Filament/Pages/Activities.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Noxo\FilamentActivityLog\Pages\ListActivities;
+
+class Activities extends ListActivities
+{
+    protected static ?string $navigationGroup = 'Core';
+
+    // protected bool $isCollapsible = true;
+
+    // protected bool $isCollapsed = false;
+
+    // public function getTitle(): string
+    // {
+    //     return __('filament-activity-log::activities.title');
+    // }
+
+    // public static function getNavigationLabel(): string
+    // {
+    //     return __('filament-activity-log::activities.title');
+    // }
+
+}
